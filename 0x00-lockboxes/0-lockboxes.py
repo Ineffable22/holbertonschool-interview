2bbb2668c6439970f71e907df4cf43234e92bb43
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-08-01 12:37:14 -0500

    Add: New file 0-lockboxes.py

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+
+def add_keys(key, add, list_box):
+    """Add a new key"""
+    if type(add) is list:
+        for i in add:
+            if i not in key and i in list_box:
+                key.append(i)
+    else:
+        if add not in key and i in list_box:
+            key.append(add)
+    return key
+
+
+def validate(k, boxes):
+    """Validate Index Error"""
+    try:
+        boxes[k]
+        return 0
+    except IndexError:
+        return k
+
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened"""
+    len_box = len(boxes)
+    list_box = list(range(1, len_box))
+    keys = []
+    new_keys = []
+    keys = add_keys(keys, boxes[0], list_box)
+
+    while (keys != [] and list_box != []):
+        for j in range(len(keys)):
+            if (validate(j, boxes) != 0):
+                print("=====ERROR=====")
+                del keys[j]
+            else:
+                new_keys = add_keys(keys, boxes[keys[j]], list_box)
+
+                if new_keys[j] != list_box[j] and new_keys[j] in list_box:
+                    del list_box[j]
+                    list_box.remove(new_keys[j])
+                else:
+                    del list_box[j]
+                del new_keys[j]
+                break
+        keys = new_keys
+    if list_box == []:
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    canUnlockAll(boxes)
