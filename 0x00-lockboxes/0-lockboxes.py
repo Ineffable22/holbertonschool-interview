e0075589006b53c8eaee46d821197229c4055e13
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-08-03 14:50:46 -0500

    Feat(New case): code was corrected 0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 """ Lockboxes """
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+logging.disable(logging.CRITICAL)
 
 
 def add_keys(key, add, list_box):
@@ -25,6 +29,7 @@ def validate(k, boxes):
 
 def canUnlockAll(boxes):
     """Method that determines if all the boxes can be opened"""
+    logging.debug("Start")
     len_box = len(boxes)
     list_box = list(range(1, len_box))
     keys = []
@@ -32,20 +37,34 @@ def canUnlockAll(boxes):
     keys = add_keys(keys, boxes[0], list_box)
 
     while (keys != [] and list_box != []):
+        logging.debug(f"WHILE")
+        logging.debug(f"boxes => {boxes}")
+        logging.debug(f"List_box => {list_box}")
         for j in range(len(keys)):
+            logging.debug(f"FOR")
+            logging.debug(f"keys => {keys}")
+            logging.debug(f"List_box => {list_box}")
             if (validate(j, boxes) != 0):
+                logging.debug(f"DEL => {keys[j]}")
                 del keys[j]
             else:
                 new_keys = add_keys(keys, boxes[keys[j]], list_box)
+                logging.debug(f"keys => {keys}")
+                logging.debug(f"new_keys[j] => {new_keys[j]}")
+                logging.debug(f"boxes[keys[j]] => {boxes[keys[j]]}")
+                logging.debug(f"List_box => {list_box}")
+                logging.debug(f"List_box[j] => {list_box[j]}")
                 if new_keys[j] != list_box[j] and new_keys[j] in list_box:
-                    del list_box[j]
                     list_box.remove(new_keys[j])
+                    logging.debug(f"DEL1 List_box => {list_box}")
                 else:
                     del list_box[j]
+                    logging.debug(f"DEL2 List_box => {list_box}")
                 del new_keys[j]
+                logging.debug(f"DEL keys => {keys}")
                 break
         keys = new_keys
-    if list_box == []:
+    if len(list_box) == 0:
         return True
     return False
 
