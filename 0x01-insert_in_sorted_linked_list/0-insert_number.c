367d4a6629b056c85fe9cd042ec3c9c49b3f119f
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-08-10 16:57:27 -0500

    Feat(Return): Function insert_node return node address 0-insert_number.c

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -17,14 +17,12 @@ listint_t *insert_node(listint_t **head, int number)
 		if (node == NULL)
 			return (NULL);
 		node->n = number;
+		node->next = NULL;
 		if (*head)
 			node->next = (*head);
-		else
-			node->next = NULL;
 		(*head) = node;
-		return (*head);
+		return (node);
 	}
 	else
-		(*head)->next = (insert_node(&((*head)->next), number));
-	return (*head);
+		return (insert_node(&((*head)->next), number));
 }
