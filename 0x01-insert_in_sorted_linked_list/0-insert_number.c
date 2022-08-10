4223091f1fbaf4f94e08cbae38db3780dc2a1285
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-08-10 16:38:55 -0500

    Feat(Mode Low-Level): Code was optimized 0-insert_number.c

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -3,30 +3,28 @@
 /**
  * insert_node - inserts a number into a sorted singly linked list.
  * @head: double pointer of singly linked list.
- * @number: index of node to insert
+ * @number: value of node to insert
  *
  * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node = NULL, *tmp = *head, *prev = NULL;
+	listint_t *node = NULL;
 
-	node = calloc(1, sizeof(listint_t));
-	if (node == NULL)
-		return (NULL);
-	node->n = number;
-	node->next = NULL;
-
-	if (number == 0)
+	if ((*head) == NULL || (*head)->n > number)
 	{
-		if ((*head) != NULL)
+		node = calloc(1, sizeof(listint_t));
+		if (node == NULL)
+			return (NULL);
+		node->n = number;
+		if (*head)
 			node->next = (*head);
+		else
+			node->next = NULL;
 		(*head) = node;
 		return (*head);
 	}
-	for (; tmp->n < number; tmp = tmp->next)
-		prev = tmp;
-	prev->next = node;
-	node->next = tmp;
+	else
+		(*head)->next = (insert_node(&((*head)->next), number));
 	return (*head);
 }
