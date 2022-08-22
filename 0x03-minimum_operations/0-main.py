4e8e63f48140098995ba0d6c2e352d117815bc34
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-08-22 02:33:35 -0500

    Add: New file 0-main.py

---
+++ 0x03-minimum_operations/0-main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = -1
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 0
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 1
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 2
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 3
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 5
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 6
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 7
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 8
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 9
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 10
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 11
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 13
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 14
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
