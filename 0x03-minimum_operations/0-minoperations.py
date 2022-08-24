3016347f6e2b61401160dc291fc9ab442d5f382c
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-08-24 15:24:52 -0500

    Feat(PEP8): top comment changed to triple double quotes 0-minoperations.py

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-# This calculates the fewest number of operations needed
-# to result in exactly n H characters in the file.
+"""
+This calculates the fewest number of operations needed
+to result in exactly n H characters in the file.
+"""
 
 
 def minOperations(n):
