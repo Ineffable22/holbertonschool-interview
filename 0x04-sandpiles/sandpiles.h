a229ed388c524002a9837d9d88fcbc2ed02db1d5
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-08-30 16:21:28 -0500

    Feat(Task 0): Specific calculations created sandpiles.h

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef _SANDPILES_H_
+#define _SANDPILES_H_
+
+#include <stdio.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void print_grid1(int grid[3][3]);
+int will_topple(int grid1[3][3]);
+
+#endif
