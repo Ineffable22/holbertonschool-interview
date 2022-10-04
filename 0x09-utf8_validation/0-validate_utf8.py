8382c5879c5ee645e12675709f3431cb5b22d260
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-10-04 18:15:26 -0500

    Add: New file 0-validate_utf8.py

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Determines if a given data set represents a valid UTF-8 encoding."""
+
+
+def validUTF8(data: list) -> bool:
+    """
+    validUTF8 - Determines if a given data set represents a valid UTF-8
+    encoding
+
+    Args:
+        @data: List of integers to identify
+    Return:
+        True if data is a valid UTF-8 encoding, else return False
+    """
+    if type(data) is not list or not all([isinstance(i, int) for i in data]):
+        return False
+
+    for num in data:
+        n = num & 0xFF
+        if num < 0:
+            if n != num + 256:
+                return False
+        else:
+            if n != num:
+                return False
+    return True
+
+
+if __name__ == '__main__':
+    validUTF8()
