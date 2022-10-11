d8fa052e472a24de7b322544fcd2c5ffa3e3e3b5
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-10-10 23:28:39 -0500

    Add: New file slide_line.h

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,11 @@
+#ifndef _slide_line_H_
+#define _slide_line_H_
+
+#include <stdio.h>
+
+#define SLIDE_LEFT 1
+#define SLIDE_RIGHT -1
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif
