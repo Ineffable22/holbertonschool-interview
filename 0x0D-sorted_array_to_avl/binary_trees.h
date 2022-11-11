ab0b8c27c0b041d4aa015b82f4920f5c1a4ef370
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-11-10 20:45:03 -0500

    Add: New file binary_trees.h

---
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -0,0 +1,32 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
+
+/* ----- binary_tree_print.c -----  */
+void binary_tree_print(const binary_tree_t *tree);
+
+/* ----- 0-sorted_array_to_avl.c -----  */
+avl_t *sorted_array_to_avl(int *array, size_t size);
+
+#endif
