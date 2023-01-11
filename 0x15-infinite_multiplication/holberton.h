3a590341c442d499d9e7bb46a98e717ff7249de5
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-01-10 23:42:32 -0500

    Add: New file holberton.h

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdlib.h>
+
+int _putchar(char c);
+void printer(char *str, int len);
+char is_digit(char *str);
+int _strlen(char *str);
+int multiplication(char *num1, char *num2);
+
+#endif /* _HOLBERTON_H_ */
