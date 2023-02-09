45bc98e08e962b92d6ed2dc5554904104d3d51c5
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-02-08 23:26:10 -0500

    Add: New file sort.h

---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,10 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+#endif /* _SORT_H_ */
