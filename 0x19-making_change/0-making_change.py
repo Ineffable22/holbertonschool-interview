e58f0d421f4b04e38ca6375cc0ba8d729cabcccf
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-02-20 11:23:41 -0500

    Add: New file 0-making_change.py

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Change comes from within
+Time Complexity O(n * log(n))
+Spatial complexity O(1)
+"""
+
+
+def makeChange(coins: list, total: int) -> int:
+    """
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+
+    Args:
+        coins (list): Is a list of the values of the coins in
+                      your possession
+        total (int):  Total number to reach
+
+    Returns:
+        int: Fewest number of coins needed to meet total
+    """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    number = 0
+    for i in coins:
+        while i <= total:
+            total -= i
+            number += 1
+        if total == 0:
+            return number
+    return -1
+
+
+if __name__ == "__main__":
+    makeChange(coins, total)
