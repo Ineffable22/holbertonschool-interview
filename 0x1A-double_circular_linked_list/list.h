f8c7e554f1eb839e47892b1b6c7e90de31dac9d1
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-02-28 12:15:13 -0500

    Feat(Task0): Task complete

---
+++ 0x1A-double_circular_linked_list/list.h
@@ -0,0 +1,26 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct List - doubly linked list
+ * @str: string - (malloc'ed string)
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct List
+{
+	char *str;
+	struct List *prev;
+	struct List *next;
+} List;
+
+List *add_node_end(List **list, char *str);
+List *add_node_begin(List **list, char *str);
+
+#endif /* _LIST_H_ */
