dd76d9d7f7d3f272a8f65c7b2d5e26430bb77c71
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-03-27 21:24:16 -0500

    Add: New file 0-wildcmp.c

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - Compares two strings and returns 1 if the strings can be
+ * considered identical, otherwise return 0.
+ * @s1: String
+ * @s2: String with wildcard to compare with s1
+ *
+ * Return: 1 if the strings can be considered identical, otherwise return 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == 0 && *s2 == 0)
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+		return (1);
+	return (0);
+}
