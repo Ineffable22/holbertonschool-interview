be39ecaafa554b5b6baa64c9de14d6c70bb4877b
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-03-29 14:59:23 -0500

    Feat(Update): New implementation

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -14,7 +14,7 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
-		return (1);
-	return (0);
+	if (*s2 != '*')
+		return (0);
+	return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 }
